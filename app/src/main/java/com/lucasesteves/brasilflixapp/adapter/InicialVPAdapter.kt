aef71056108c37740b683ca2329a1bea590bc8e8
Lucas Esteves <lucas.coelho9280@gmail.com>
2021-07-27 01:59:29 -0300

    ImplementaçãoLogin

---
+++ app/src/main/java/com/lucasesteves/brasilflixapp/adapter/InicialVPAdapter.kt
@@ -0,0 +1,19 @@
+package com.lucasesteves.brasilflixapp.adapter
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class InicialVPAdapter(
+    private val fragmentsList: List<Fragment>,
+    private val fragmentsTitleList: List<String>,
+    val fragmentManager: FragmentManager
+): FragmentPagerAdapter(fragmentManager) {
+
+    override fun getCount() = fragmentsList.size
+
+    override fun getItem(position: Int) = fragmentsList[position]
+
+    override fun getPageTitle(position: Int) = fragmentsTitleList[position]
+}
+
---
+++ app/src/main/java/com/lucasesteves/brasilflixapp/fragments/inicialFragment.kt
@@ -0,0 +1,53 @@
+package com.lucasesteves.brasilflixapp.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.lucasesteves.brasilflixapp.R
+import com.lucasesteves.brasilflixapp.adapter.InicialVPAdapter
+import com.lucasesteves.brasilflixapp.databinding.FragmentInicialBinding
+
+
+class inicialFragment : Fragment() {
+
+    private var binding: FragmentInicialBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val fragmentsList = listOf(LoginFragment(), CadastrarFragment())
+        val fragmentsTitleList = listOf("Login", "Cadastro")
+
+        activity?.let {
+            val viewPagerAdapter = InicialVPAdapter(
+                fragmentManager = childFragmentManager,
+                fragmentsList = fragmentsList,
+                fragmentsTitleList = fragmentsTitleList
+            )
+            binding?.let { bindingNonNull ->
+                with(bindingNonNull) {
+                    vpPages.adapter = viewPagerAdapter
+                    tabLayout.setupWithViewPager(vpPages)
+
+                }
+            }
+        }
+
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentInicialBinding.inflate(inflater, container, false)
+        return binding?.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
+
+
+}
