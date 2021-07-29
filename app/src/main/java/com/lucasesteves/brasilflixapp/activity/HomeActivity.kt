557f905862280b0d51e85019288452013eef5e0d
Lucas Esteves <lucas.coelho9280@gmail.com>
2021-07-29 02:16:55 -0300

    FinalizandoTelaHome

--- app/src/main/java/com/lucasesteves/brasilflixapp/activity/HomeActivity.kt
+++ app/src/main/java/com/lucasesteves/brasilflixapp/activity/HomeActivity.kt
@@ -2,11 +2,30 @@ package com.lucasesteves.brasilflixapp.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.PopupMenu
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.lucasesteves.brasilflixapp.R
+import com.lucasesteves.brasilflixapp.databinding.ActivityHomeBinding
+import me.ibrahimsn.lib.SmoothBottomBar
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+//        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+//        setupSmoothBottomMenu(navController)
     }
+
+//    private fun setupSmoothBottomMenu(navController: NavController) {
+//        val popupMenu = PopupMenu(this, null)
+//        popupMenu.inflate(R.menu.navigation_menu)
+//        val menu = popupMenu.menu
+//        binding.bottomBar.setupWithNavController(menu, navController)
+//    }
 }
---
+++ app/src/main/java/com/lucasesteves/brasilflixapp/model/filmes.kt
@@ -0,0 +1,12 @@
+package com.lucasesteves.brasilflixapp.model
+
+import com.google.gson.annotations.SerializedName
+
+data class filmes(
+    @SerializedName("poster_path")
+    var poster_path : String,
+    @SerializedName("release_date")
+    var release_date : String,
+    @SerializedName("title")
+    var title : String
+){}
