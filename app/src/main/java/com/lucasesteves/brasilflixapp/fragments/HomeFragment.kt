abc87fdcbfb0f79ceb6be53eef64f2e8b060de7f
Lucas Esteves <lucas.coelho9280@gmail.com>
2021-07-29 02:20:32 -0300

    FinalizandoTelaHome

--- app/src/main/java/com/lucasesteves/brasilflixapp/fragments/HomeFragment.kt
+++ app/src/main/java/com/lucasesteves/brasilflixapp/fragments/HomeFragment.kt
@@ -6,11 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.lucasesteves.brasilflixapp.R
-
+import com.lucasesteves.brasilflixapp.databinding.FragmentHomeBinding
+import com.lucasesteves.brasilflixapp.databinding.FragmentInicialBinding
 
 
 class HomeFragment : Fragment() {
 
+    private var binding: FragmentHomeBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,8 +23,21 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
     }
 
 
